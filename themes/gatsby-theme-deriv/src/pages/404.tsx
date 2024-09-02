58e776e59c23529c298f734d0777211e8fd1f19d
behnaz-deriv <behnaz@regentmarkets.com>
2024-09-02 16:44:45 +0800

    chore: remove strapi and dependencies

--- themes/gatsby-theme-deriv/src/pages/404.tsx
+++ themes/gatsby-theme-deriv/src/pages/404.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { graphql } from 'gatsby'
 import { WithIntl } from 'components/localization'
 import PageNotFound from 'features/pages/404'
 import { isBrowser } from 'common/utility'
@@ -11,83 +10,6 @@ const NotFound = ({ pageContext }: TGatsbyHead) => {
     return isBrowser() && <PageNotFound region={region} />
 }
 
-export const query = graphql`
-    query {
-        strapiWhoWeArePage {
-            hero {
-                header
-                sub_header
-                first_paragraph
-                second_paragraph
-                third_paragraph
-            }
-            our_values {
-                header
-                values {
-                    header
-                    sub_header
-                }
-            }
-            our_principles {
-                header
-                principles {
-                    header
-                    sub_header
-                }
-                button {
-                    link_name
-                }
-            }
-            our_leadership {
-                header
-                leaders {
-                    role
-                }
-            }
-            deriv_in_numbers {
-                header
-                sub_header
-                numbers {
-                    description
-                }
-            }
-            our_locations {
-                header
-                numbers {
-                    description
-                }
-            }
-            banner {
-                header
-                sub_header
-                link_name
-            }
-        }
-        strapiContactUs {
-            hero {
-                header
-                sub_header
-            }
-            support_section {
-                header
-                sub_header
-                link_title
-            }
-            our_offices
-            office_address {
-                continent
-                country {
-                    country_name
-                }
-            }
-            business_partnership {
-                header
-                sub_header
-                live_chat_title
-            }
-        }
-    }
-`
 
 export default WithIntl()(NotFound)
 
