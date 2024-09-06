42fdfed5b5d947cc2ea34780e9a488e1522c3510
behnaz-deriv <behnaz@regentmarkets.com>
2024-09-06 13:24:06 +0800

    chore: referrer_toke to api

--- themes/gatsby-theme-deriv/src/common/url-affiliate-token.ts
+++ themes/gatsby-theme-deriv/src/common/url-affiliate-token.ts
@@ -4,8 +4,5 @@ export const affiliateTokenURL = () => {
         const query_string = window.location.search
         const url_params = new URLSearchParams(query_string)
         const aff_token = url_params.get('t')
-       // if (aff_token) {
-        //    localStorage.setItem(referrer_token, aff_token)
-       // }
        return aff_token
 }
