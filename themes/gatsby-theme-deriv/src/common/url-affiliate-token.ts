94ebfae27c35b579f4c74ef81c5c2596d8b5f44e
behnaz-deriv <behnaz@regentmarkets.com>
2024-09-09 17:01:24 +0800

    chore: just param

--- themes/gatsby-theme-deriv/src/common/url-affiliate-token.ts
+++ themes/gatsby-theme-deriv/src/common/url-affiliate-token.ts
@@ -5,20 +5,8 @@ export const affiliateTokenURL = () => {
     const urlObject = new URL(window.location.href);
     const params = new URLSearchParams(urlObject.search);
     const token = params.get("t");
-    const url_cookies_seperator = '&'
     if (params.has("referrer") && token != null) 
-        return  extractUpToSecondOccurrence(token,url_cookies_seperator )
- 
+        return token
 }
 
-const extractUpToSecondOccurrence = (str:string , char:string): string => {
-    const firstIndex = str.indexOf(char);
-    if (firstIndex === -1) {
-        return str; 
-    }
-    const secondIndex = str.indexOf(char, firstIndex + 1);
-    if (secondIndex === -1) {
-        return str.substring(0, firstIndex + 1); 
-    }
-    return str.substring(0, secondIndex);
-}
+
