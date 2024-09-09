92e200c99bdefdcccbe456dc32f0a95def9ce8e3
behnaz-deriv <behnaz@regentmarkets.com>
2024-09-09 12:54:28 +0800

    chore: instead of window use isbrowser

--- themes/gatsby-theme-deriv/src/common/url-affiliate-token.ts
+++ themes/gatsby-theme-deriv/src/common/url-affiliate-token.ts
@@ -1,6 +1,8 @@
 
+import { isBrowser } from './utility'
 export const affiliateTokenURL = () => {
-    if (!window) return;
+    //if (!window) return;isBrowser
+    if (!isBrowser()) return;
         const query_string = window.location.search
         const url_params = new URLSearchParams(query_string)
         const aff_token = url_params.get('t')
