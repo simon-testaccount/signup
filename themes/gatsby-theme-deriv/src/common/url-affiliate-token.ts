47976eedf5f53932f1f44b737738bdfa8e4d4390
behnaz-deriv <behnaz@regentmarkets.com>
2024-09-09 16:36:18 +0800

    chore: get token till second &

--- themes/gatsby-theme-deriv/src/common/url-affiliate-token.ts
+++ themes/gatsby-theme-deriv/src/common/url-affiliate-token.ts
@@ -1,10 +1,24 @@
 
 import { isBrowser } from './utility'
 export const affiliateTokenURL = () => {
-    //if (!window) return;isBrowser
     if (!isBrowser()) return;
-        const query_string = window.location.search
-        const url_params = new URLSearchParams(query_string)
-        const aff_token = url_params.get('t').substring(0,31)
-       return aff_token
+    const urlObject = new URL(window.location.href);
+    const params = new URLSearchParams(urlObject.search);
+    const token = params.get("t");
+    const url_cookies_seperator = '&'
+    if (params.has("referrer") && token != null) 
+        return  extractUpToSecondOccurrence(token,url_cookies_seperator )
+ 
+}
+
+const extractUpToSecondOccurrence = (str:string , char:string): string => {
+    const firstIndex = str.indexOf(char);
+    if (firstIndex === -1) {
+        return str; 
+    }
+    const secondIndex = str.indexOf(char, firstIndex + 1);
+    if (secondIndex === -1) {
+        return str.substring(0, firstIndex + 1); 
+    }
+    return str.substring(0, secondIndex);
 }
